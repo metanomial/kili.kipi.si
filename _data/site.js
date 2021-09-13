52e52dc0bcbcccb2da1fd7eb234160e15e88a51b
Benjamin <benjamin@metanomial.com>
2021-09-13 16:16:32 -0700

    o pali e telo tawa pi lipu sin

---
+++ _data/site.js
@@ -0,0 +1,7 @@
+export const title = "kili kipisi";
+export const description = "toki musi pi jan Pensamin";
+export const author = {
+  name: "jan Pensamin",
+  email: "benjamin@metanomial.com",
+  url: "https://metanomial.com",
+};
