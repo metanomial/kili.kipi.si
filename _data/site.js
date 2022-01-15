98b0dd2fbbf3b1f6fa25bddf58e2b19462c38cf5
Benjamin <benjamin@metanomial.com>
2022-01-14 21:30:15 -0800

    o tawa e sona lipu

--- _data/site.js
+++
@@ -1,7 +0,0 @@
-export const title = "kili kipisi";
-export const description = "toki musi pi jan Pensamin";
-export const author = {
-  name: "jan Pensamin",
-  email: "benjamin@metanomial.com",
-  url: "https://metanomial.com",
-};
